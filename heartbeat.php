8213f457c531a4f29cb5cef951ca118192938a6c
Systwo <starfruitinc@gmail.com>
2014-09-20 19:29:21 -0400

    Add heartbeat.php

---
+++ heartbeat.php
@@ -0,0 +1,16 @@
+<?php
+$db = new PDO("mysql:host=localhost;dbname=basket;charset=utf8", "root", "");
+
+$preStmt = $db->prepare("SELECT * FROM clients WHERE clientip=:ip LIMIT 1");
+
+$preStmt->execute(array("ip" => $_SERVER["REMOTE_ADDR"]));
+$row = $preStmt->fetch(PDO::FETCH_ASSOC);
+echo print_r($row);
+if ($row) {
+	$db->prepare("UPDATE clients SET lastheartbeat=NOW() WHERE clientip=:id")->execute(array("id" => $row['clientid']));
+} else {
+	$db->prepare("INSERT INTO clients (lastheartbeat, clientip) VALUES (NOW(), :clientip)")->execute(array("clientip" => $_SERVER['REMOTE_ADDR']));
+}
+
+
+?>
