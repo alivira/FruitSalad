dab0e9e35c209e4b99ae98b7f09284ad5b4b7960
Systwo <starfruitinc@gmail.com>
2014-09-20 17:07:42 -0400

    Added result POSTing to tracker

--- tracker.php
+++ tracker.php
@@ -1,9 +1,9 @@
 <?php
 	
 
+$db = new PDO("mysql:host=localhost;dbname=basket;charset=utf8", "root", "");
 //Get job
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	$db = new PDO("mysql:host=localhost;dbname=basket;charset=utf8", "root", "");
 	$stmt = $db->query("SELECT * FROM jobs WHERE (jobstatus='INCOMPLETE') OR (jobstatus='INPROGRESS' AND NOW() - `lastsent` > 3600) LIMIT 1");
 
 
@@ -11,11 +11,29 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	header("Content-type: application/json");
 	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		//Update job's lastsent
-		$db->exec("UPDATE jobs SET lastsent=NOW() WHERE jobid=" . $row['jobid']);
+		$db->prepare("UPDATE jobs SET lastsent=NOW() WHERE jobid=:jobid")->execute(array(":jobid" => $row['jobid']));
 		echo json_encode($row);
 		echo "\n";
 	}
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (strlen(file_get_contents('php://input')) == 0 ||
+	    !isset($_GET['jobid'])) {
+	        header('HTTP/1.1 400 Bad request');
+		return;
+	}
+
+
+	//Prepare sql statements
+	$preStmt = $db->prepare("UPDATE jobs SET jobresult=:result WHERE jobid=:jobid", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+
+	//Execute it
+	$preStmt->execute(array(":result" => file_get_contents('php://input'), ":jobid" => $_GET['jobid']));
+	if ($preStmt->rowCount() > 0) {
+	        header('HTTP/1.1 200 OK');
+	} else {
+		header('HTTP/1.1 404 Not Found');
+	}
 }
 
 
