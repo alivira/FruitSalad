70205273b6eda2ce32c80fdac804805cf3320152
Erik Derohanian <erik@erik-j.de>
2014-09-20 12:39:29 -0400

    Add solver and heartbeat skeletons

---
+++ website/js/solver.js
@@ -0,0 +1,38 @@
+function Runner(){
+    var codeBlock, data;
+    var sleepTime = 0;
+}
+
+//option needs to be whatever option the user selects (cancer, aliens, etc)
+Runner.prototype.getFunction = function(option){
+
+}
+
+Runner.prototype.go(option){
+    this.getFunction(option);
+    this.execute();
+}
+
+Runner.prototype.getData = function(){
+
+}
+
+Runner.prototype.execute = function(){
+
+    var that = this;
+    var runUnit = function(){
+        this.data = this.getData();
+        var before = new Date(); before = before.getTime();
+        this.codeBlock.call(this.data);
+        var after = new Date(); after = after.getTime();
+        var timeDiff = after - before;
+        that.sleepTime = 1000 * timeDiff;
+    }
+
+    while(1){
+        setTimeout(runUnit, sleepTime);
+    }
+}
+
+runner = new Runner();
+var heartbeat = new Worker("heartbeatWorker.js");
