3375cba54804db2670c36c6f0703dda388fb937a
Graham Bleaney <gbleaney@gmail.com>
2014-09-20 21:46:27 -0400

    Fixed password

--- taskHelpers/factor-createJobs.php
+++ taskHelpers/factor-createJobs.php
@@ -1,7 +1,7 @@
 <?php
 	$NUM_TO_FACTOR = 1000;
 	$NUM_PER_JOB = 100;
-	$db = new PDO("mysql:host=localhost;dbname=basket;charset=utf8", "root", "");
+	$db = new PDO("mysql:host=localhost;dbname=basket;charset=utf8", "root", "What is systems?");
 	for ($i = 0; $i < $NUM_TO_FACTOR / $NUM_PER_JOB; $i++) {
 		$db->exec("INSERT INTO jobs (jobdata, jobstatus) VALUES ('" . $i * $NUM_PER_JOB . "," . ((($i + 1) * $NUM_PER_JOB) - 1) . "', 'INCOMPLETE')");
 		//echo "INSERT INTO jobs (jobdata, jobstatus) VALUES ('" . $i * $NUM_PER_JOB . "," . ((($i + 1) * $NUM_PER_JOB) - 1) . "', INCOMPLETE)";
